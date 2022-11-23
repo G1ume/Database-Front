6080d2cd5451013cac1588643083fe25964ede46
G1umeDesk <20231088@buaa.edu.cn>
2022-11-23 16:23:52 +0800

    不报错

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
-
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import {createApp} from 'vue'
 import ElementUI from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 
 import App from './App'
-import router from './router'
+import router from './router/index'
 
-Vue.config.productionTip = false;
+//Vue.config.productionTip = false;
 
-Vue.use(ElementUI)
-
+//Vue.use(ElementUI)
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
 /* eslint-disable no-new */
+/*
 var app = new Vue({
     el: '#app',
-    /* 创建和挂载根实例 */
+    // 创建和挂载根实例
     router,
-    components: { App },
+    components: {App},
     template: '<App/>'
 })
+*/
+
+
+
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,33 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+//import Router from 'vue-router'
 import Login from '@/components/Login.vue'
 import Main from '@/components/allMain.vue'
 
-Vue.use(Router)
+import {createRouter, createWebHistory} from 'vue-router'
 
-export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'Login',
-            component: Login,
-            meta:{
-                keepalive:false
-            }
-        },
-        {
-            path: '/Main',
-            name: 'Main',
-            component: Main,
-            meta:{
-                keepalive:true
-            }
+const routerHistory = createWebHistory()
+const routes = [{
+    path: '/',
+    name: 'Login',
+    component: Login,
+    meta: {
+        keepalive: false
+    }
+},
+    {
+        path: '/Main',
+        name: 'Main',
+        component: Main,
+        meta: {
+            keepalive: true
         }
-    ]
+    }]
+
+const router = createRouter({
+    history: routerHistory,
+    routes
 })
+
+export default router
+//Vue.use(Router)
 
