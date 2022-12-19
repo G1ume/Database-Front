bf751ccd08079af10ca18bbb389318be624fbaf5
11858 <1@>
2022-12-19 15:33:27 +0800

    增加了动态加载和商品初始化以及商品查询和后端的接口函数的布局

--- src/components/Good.vue
+++ src/components/Good.vue
@@ -14,32 +14,35 @@
           </el-checkbox-button>
         </el-checkbox-group>
       </div>
-
-     各个商品信息，类似那20个问题，每个商品card都对应链接
       <el-button @click="query">查询</el-button>
       <el-divider/>
-        <el-row >
-          <el-col
-              v-for="(o, index) in 24"
-              :key="o"
-              :span="5.5"
-              :offset="1"
-          >
-            <el-card :body-style="{ padding: '2px' }" >
-              <img
-                  src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-                  class="image"
-              />
-              <div style="padding: 14px">
-                <span>Yummy hamburger</span>
-                <div class="bottom">
-                  <el-button text class="button" @click="singleobj(index)">查看详情</el-button>
-                </div>
+      <el-row type="flex">
+        <el-col
+            v-for="(item, index) in clothe24"
+            :key="index"
+            :span="5.5"
+            :offset="1"
+        >
+
+
+          <el-card :body-style="{ padding: '2px' }">
+
+            <img
+                :src="item.cpi"
+                width="50px"
+                class="image"
+             alt=""
+            />
+            <div style="padding: 14px" >
+              <span>Yummy hamburger</span>
+              <div class="bottom">
+                <el-button text class="button" @click="singleobj(index)">查看详情</el-button>
               </div>
-            </el-card>
-            <h6></h6>
-          </el-col>
-        </el-row>
+            </div>
+          </el-card>
+          <h6></h6>
+        </el-col>
+      </el-row>
     </el-main>
     <el-footer>
       //翻页部分
@@ -68,42 +71,124 @@
 }
 </style>
 <script>
+import qs from "qs";
+
 export default {
   data() {
     return {
+      clist:[
+          "https://pic2.zhimg.com/1ccca1f0ff810d3a319206114c1174a1_r.jpg?source=1940ef5c",
+          "https://pic3.zhimg.com/v2-fb4a711b0e98565b5046fe532d33cf32_r.jpg",
+        "https://gss0.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/faf2b2119313b07e3ca515b10cd7912397dd8c6b.jpg",
+        "https://img.zcool.cn/community/016bb45aae0236a80120be14708b57.jpg@1280w_1l_2o_100sh.jpg",
+        "https://img.zcool.cn/community/01ec3c5de8caffa8012095687d15db.jpg@1280w_1l_2o_100sh.jpg",
+        "https://ts1.cn.mm.bing.net/th/id/R-C.80fec10a165fffba132f12d51145c2e3?rik=Fgcgx9%2ba6M%2bLWg&riu=http%3a%2f%2fimg.china.alibaba.com%2fimg%2fibank%2f2013%2f489%2f359%2f1059953984_198386960.jpg&ehk=OgnZ3zvIBbd%2fhOqAbYogVwiIttVwOshRPMsOgMgjdyM%3d&risl=&pid=ImgRaw&r=0",
+        "https://img95.699pic.com/element/40108/0378.png_860.png",
+        "https://img.alicdn.com/bao/uploaded/i1/2351667738/O1CN01pwKfXO2723NgMMcRx_!!0-item_pic.jpg",
+        "https://img.alicdn.com/bao/uploaded/i3/1081711059/O1CN01rKidHr1Jh409lCeh9_!!1081711059.jpg",
+        "https://t00img.yangkeduo.com/goods/images/2020-04-26/1a7189885390dc552e30e77ecca3dcc6.jpeg"
+
+
+      ],
       checkTypeList: [],
       checkboxGroup1: 0,
-      clothe1:[
-        {label:"上衣",value:1},
-        {label:"长袖",value:2},
-        {label:"短袖",value:3},
-        {label:"下装",value:4},
-        {label:"长裤",value:5},
-        {label:"短裤",value:6},
-        {label:"鞋",value:0},
+      clothe1: [
+        {label: "上衣", value: 1},
+        {label: "长袖", value: 2},
+        {label: "短袖", value: 3},
+        {label: "下装", value: 4},
+        {label: "长裤", value: 5},
+        {label: "短裤", value: 6},
+        {label: "鞋", value: 0},
       ],
-      o:[
-        {img:""},
-        {name:""}
-      ]
+      o: {cid: "",cpid: "",cpi: "",cpr: 0,cn: "",cde: "",cnum: 0},
+      clothe24: []
     }
   }
   ,
   methods: {
     query: function () {
+      let ccolist=[]
       for (let index = 0; index < this.checkTypeList.length; index++) {
         let item = this.checkTypeList[index];
-        console.log("item",item);
+        console.log("item", item);
+        ccolist.push(item)
         //dubug使用，正式的使用可以直接发送 checkTypeList到后端
       }
+      let cco1=ccolist.join()
+      console.log("cco1",cco1)
+      this.$axios({
+        methods: 'post',
+        url: '/query/',
+        data: qs.stringify({
+          cco: cco1,
+        }),
+        timeout: 1000,
+      })
+          .then(res => {
+            this.clothe24=[]
+            for (let i = 0; i < res.data.list.length; i++) {
+              this.o.cid = res.data.list[i].cid
+              this.o.cpid = res.data.list[i].cpid
+              this.o.cpr = res.data.list[i].cpr
+              this.o.cn = res.data.list[i].cn
+              this.o.cde = res.data.list[i].cde
+              this.o.cnum = res.data.list[i].cnum
+              this.clothe24.add(this.o)
+            }
+            //debug
+            for (let i = 0; i < 2; i++) {
+              this.o.cpr = i
+              this.clothe24.push(this.o)
+            }
+          })
+          .catch(err => {
+                console.log(err);
+                for (let i = 0; i < this.clist.length; i++) {
+                  let m= {}
+                  m.cid =  "00"+i
+                  m.cpid = "10"+i
+                  m.cpr = i
+                  m.cn = "衣服"+i
+                  m.cde = "这是衣服"+i
+                  m.cnum = i*100
+                  m.cpi=this.clist.at(i)
+                  this.clothe24.push(m)
+                  console.log("+1")
+                }
+                console.log("num of clothe",this.clothe24.length)
+                console.log("query end")
+              }
+          )
+
       //console.log(this.$route.query.password)
       //console.log(pwd)
     },
-    singleobj:function () {
-
+    singleobj: function (index) {
+      console.log(typeof index, index)
       //跳转到特定的
-      this.$router.push({name:'Login'})
+      // this.$router.push({name:'Login'})
+
+      this.o = clothe24.at(index)
+      this.$router.push(
+          {
+            name: "singlegood", query: {
+              cid: o.cid,
+              cpid: o.cpid,
+              cpi: o.cpi,
+              cpr: o.cpr,
+              cn: o.cn,
+              cde: o.cde,
+              cnum: o.cnum
+            }
+          }
+      )
     }
+  },
+  created() {
+    this.query()
+    console.log("num in created",this.clothe24.length)
+    console.log("created end")
   }
 }
 </script>
---
+++ src/components/singlegood.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-container>
+
+  </el-container>
+</template>
+<style>
+
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      o:[
+        {img:""},
+        {name:""}
+      ]
+    }
+  }
+  ,
+  methods: {
+    query: function () {
+      //console.log(this.$route.query.password)
+      //console.log(pwd)
+    },
+
+  },
+  created() {
+
+  }
+}
+</script>
