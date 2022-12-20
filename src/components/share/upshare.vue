7b342050e16598dd95e1c1289e39866afa074842
G1ume <20231088@buaa.edu.cn>
2022-12-21 00:00:11 +0800

    content fix

--- src/components/share/upshare.vue
+++ src/components/share/upshare.vue
@@ -1,52 +1,52 @@
 <template>
-    <el-container>
-      <el-header>
+  <el-container>
+    <el-header>
       <span style="font-size: 30px;font-weight: bold">
         分享好物
       </span>
-      </el-header>
-      <el-main>
-
-
-        <el-card :body-style="{ padding: '0px' }" shadow="hover">
-          <h6></h6>
-          <div>
-            分享的图片: <el-input placeholder="请输入分享的图片url" v-model="s.spi" clearable class="input_style"></el-input>
-          </div>
-          <div>
-            衣服的id: <el-input placeholder="请输入衣服的id" v-model="s.scid" clearable class="input_style"></el-input>元
-          </div>
-          <div>
-            分享的标题: <el-input placeholder="请输入分享的标题" v-model="s.she" clearable class="input_style"></el-input>
-          </div>
-          <div>
-            分享的描述:
-            <el-input
-                v-model="s.sde"
-                :autosize="{ minRows: 2, maxRows: 4 }"
-                type="textarea"
-                placeholder="请输入商品的描述"
-                class="input_style"
-            />
-          </div>
-
-
-          <el-checkbox-group v-model="checkTypeList" size="large">
-            <el-checkbox-button v-for="(type,index) in clothe1" :key="index" :label="type.value">
-              {{ type.label }}
-            </el-checkbox-button>
-          </el-checkbox-group>
-
-        </el-card>
+    </el-header>
+    <el-main>
+      <el-card :body-style="{ padding: '0px' }" shadow="hover">
         <h6></h6>
-        <h6></h6>
-      </el-main>
-      <el-footer>
         <div>
-          <el-button @click="upshare">上传分享</el-button>
+          分享的图片:
+          <el-input placeholder="请输入分享的图片url" v-model="s.spi" clearable class="input_style"></el-input>
         </div>
-      </el-footer>
-    </el-container>
+        <div>
+          衣服的id:
+          <el-input placeholder="请输入衣服的id" v-model="s.scid" clearable class="input_style"></el-input>
+        </div>
+        <div>
+          分享的标题:
+          <el-input placeholder="请输入分享的标题" v-model="s.she" clearable class="input_style"></el-input>
+        </div>
+        <div>
+          分享的描述:
+          <el-input
+              v-model="s.sde"
+              :autosize="{ minRows: 2, maxRows: 4 }"
+              type="textarea"
+              placeholder="请输入商品的描述"
+              class="input_style"
+          />
+        </div>
+
+        <el-checkbox-group v-model="checkTypeList" size="large">
+          <el-checkbox-button v-for="(type,index) in clothe1" :key="index" :label="type.value">
+            {{ type.label }}
+          </el-checkbox-button>
+        </el-checkbox-group>
+
+      </el-card>
+      <h6></h6>
+      <h6></h6>
+    </el-main>
+    <el-footer>
+      <div>
+        <el-button @click="upshare">上传分享</el-button>
+      </div>
+    </el-footer>
+  </el-container>
 
 
 </template>
@@ -60,7 +60,7 @@ export default {
   name: "upshare",
   data() {
     return {
-      s: {sco: "", sid: "", spi: "",sst:0, scid: "", she: "", sde: "", sti: "",spid:""},
+      s: {sco: "", sid: "", spi: "", sst: 0, scid: "", she: "", sde: "", sti: "", spid: ""},
       dialogOfUpload: false,
       fileList: [],
       headers: {
@@ -72,17 +72,17 @@ export default {
     }
   },
   methods: {
-    upshare:function () {
+    upshare: function () {
       //确认上传
-      let ccolist=[]
+      let ccolist = []
       for (let index = 0; index < this.checkTypeList.length; index++) {
         let item = this.checkTypeList[index];
         console.log("item", item);
         ccolist.push(item)
         //dubug使用，正式的使用可以直接发送 checkTypeList到后端
       }
-      let cco1=ccolist.join()
-      console.log("cco1",cco1)
+      let cco1 = ccolist.join()
+      console.log("cco1", cco1)
       let aData = new Date();
       let dateValue = aData.getFullYear() + "-" + (aData.getMonth() + 1) + "-" + aData.getDate();
       console.log(dateValue)
@@ -93,11 +93,11 @@ export default {
         data: qs.stringify({
           sco: this.s.sco,
           spi: this.s.spi,
-          scid:this.s.scid,
-          she:this.s.she,
-          sde:this.s.sde,
+          scid: this.s.scid,
+          she: this.s.she,
+          sde: this.s.sde,
           sti: dateValue,
-          spid:store.state.logInfo.user_id
+          spid: store.state.logInfo.user_id
         }),
         timeout: 1000,
       })
@@ -130,10 +130,10 @@ export default {
 
     }
     ,
-    fail:function (){
+    fail: function () {
       ElMessage('分享失败,请重新检查您的提交.')
     },
-    success:function (){
+    success: function () {
       ElMessage('上架成功,即将前往个人中心.')
       this.$router.push({
         name: "PersonalCenter",
