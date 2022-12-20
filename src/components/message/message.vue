1c127f36254731180ab193d93a60a48b5bf2971f
11858 <1@>
2022-12-20 19:43:55 +0800

    修改了message的key

--- src/components/message/message.vue
+++ src/components/message/message.vue
@@ -51,8 +51,8 @@ export default {
       pageSize: store.state.pagecfg.pagesize,
       pageElemNum: 0,
       headIndex: 0,
-      message:{uid:"",	oid:""	,mid:"",	mt:"",	mc:""	,mst:"",	sid:"",	spic:""	,ppic:""},
-      message1:{uid:"1288",	oid:"3288"	,mid:"0000",	mt:"2023-1-1",	mc:"消息实例"	,mst:"未读",	sid:"1288",	spic:"https://ts1.cn.mm.bing.net/th/id/R-C.0ae5d26331217cb9296c69f12101791a?rik=9%2b2PZBDj1MLbaQ&riu=http%3a%2f%2fbpic.588ku.com%2felement_pic%2f17%2f07%2f05%2f721dde26da8f012ee82c0e221cd0a60d.jpg&ehk=04P3IpY183ObpZdId4%2fTf4Q9Wje%2bz2aXV%2fuGGcqrzo4%3d&risl=&pid=ImgRaw&r=0"	,ppic:""}
+      message:{uid:"",	oid:""	,mid:"",	mtime:"",	mcontent:""	,mst:"",	sid:"",	spic:""	,ppic:""},
+      message1:{uid:"1288",	oid:"3288"	,mid:"0000",	mtime:"2023-1-1",	mcontent:"消息实例"	,mst:"未读",	sid:"1288",	spic:"https://ts1.cn.mm.bing.net/th/id/R-C.0ae5d26331217cb9296c69f12101791a?rik=9%2b2PZBDj1MLbaQ&riu=http%3a%2f%2fbpic.588ku.com%2felement_pic%2f17%2f07%2f05%2f721dde26da8f012ee82c0e221cd0a60d.jpg&ehk=04P3IpY183ObpZdId4%2fTf4Q9Wje%2bz2aXV%2fuGGcqrzo4%3d&risl=&pid=ImgRaw&r=0"	,ppic:""}
     }
   },
   methods:{
@@ -81,12 +81,12 @@ export default {
               uid: res.data.list.at(i).uid,
               oid: res.data.list.at(i).oid,
               mid: res.data.list.at(i).mid,
-              mt: res.data.list.at(i).mt,
-              mc: res.data.list.at(i).mc,
+              mtime: res.data.list.at(i).mtime,
+              mcontent: res.data.list.at(i).mcontent,
               mst: res.data.list.at(i).mst,
-              sid: res.data.list.at(i).sid,
-              spic: res.data.list.at(i).spic,
-              ppic: res.data.list.at(i).ppic
+              sid: res.data.list.at(i).sid
+              // spic: res.data.list.at(i).spic,
+              // ppic: res.data.list.at(i).ppic
             })
           }
         }).catch(err => {
