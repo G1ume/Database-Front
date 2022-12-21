14887265baa28f8dae9ddd19603b154c6443fc11
G1ume <20231088@buaa.edu.cn>
2022-12-21 22:00:30 +0800

    登录页面完成

--- src/components/userInfo/Login.vue
+++ src/components/userInfo/Login.vue
@@ -1,21 +1,38 @@
 <template>
-  <div class="login">
-    <h1>请登录</h1>
-    <div>
-      <el-input placeholder="请输入用户名" v-model="name" clearable class="input_style">输入内容为：</el-input>
-      <span v-if="error.name" class="err-msg">{{ error.name }}</span>
-    </div>
-    <div>
-      <el-input placeholder="请输入密码" v-model="pwd" show-password class="input_style"></el-input>
-      <span v-if="error.pwd" class="err-msg">{{ error.pwd }}</span>
-    </div>
-    <div>
-      <el-button type="primary" @click="login" class="login_style">登录</el-button>
-    </div>
-    <div>
-      <p v-show="true"><span>没有账号？<el-link type="primary" @click="toRegister">立即注册</el-link></span></p>
-    </div>
-  </div>
+  <el-container>
+    <el-header>
+      <span style="font-weight:900;font-size: 30px">登录</span>
+    </el-header>
+    <el-divider/>
+    <el-main>
+      <h1>请输入邮箱和密码</h1>
+      <div>
+        <el-input size="large" placeholder="请输入邮箱" v-model="name" clearable class="input_style">输入内容为：
+        </el-input>
+        <span v-if="error.name" class="err-msg">{{ error.name }}</span>
+      </div>
+      <h3/>
+      <div>
+        <el-input size="large" placeholder="请输入密码" v-model="pwd" show-password class="input_style"></el-input>
+        <span v-if="error.pwd" class="err-msg">{{ error.pwd }}</span>
+      </div>
+      <h3/>
+      <div>
+        <el-button type="primary" @click="login" class="login_style">登录</el-button>
+      </div>
+      <p/>
+      <div>
+        <el-row :gutter="15">
+          <el-col :span="10"/>
+          <el-col :span="0.5"><span style="font-size: 16px" >没有账号？</span></el-col>
+          <el-col :span="0.5">
+            <el-link style="font-size:16px" type="primary" @click="toRegister">立即注册</el-link>
+          </el-col>
+          <el-col :span="11"/>
+        </el-row>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 <script>
 import qs from "qs"
@@ -54,7 +71,7 @@ export default {
         })
             .then(res => {
               switch (res.data.result) {
-                case 0:{
+                case 0: {
                   ElMessage.error("用户名或密码错误！")
                   break;
                 }
@@ -71,8 +88,8 @@ export default {
                   });
                   break;
                 }
-                //管理员登录成功
-                case 3:{
+                  //管理员登录成功
+                case 3: {
                   store.commit("logIn", {
                     userId: res.data.uid,
                     userName: res.data.un,
@@ -84,7 +101,7 @@ export default {
                   });
                   break;
                 }
-                //登录失败
+                  //登录失败
                 default: {
                   console.log(res.data)
                   ElMessage.error("登录失败！用户不存在")
