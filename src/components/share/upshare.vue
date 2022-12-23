64146745f87566674c3ec1e3d0557938a37861b5
G1ume <20231088@buaa.edu.cn>
2022-12-23 22:23:00 +0800

    upShare

--- src/components/share/upshare.vue
+++ src/components/share/upshare.vue
@@ -9,32 +9,72 @@
       <el-card :body-style="{ padding: '0px' }" shadow="hover">
         <h6></h6>
         <div>
-          分享的图片:
-          <el-input placeholder="请输入分享的图片url" v-model="s.spi" clearable class="input_style"></el-input>
+          <el-image :src="s.spi"
+                    style="width: 200px ;height:200px"
+                    fit="cover"/>
+          <h1/>
+          <el-button type="primary">
+            <el-upload
+                action=""
+                class="alignContainer"
+                :http-request="upload"
+                :show-file-list="false"
+            >
+              <div class="avatar-update">选择图片</div>
+            </el-upload>
+          </el-button>
         </div>
+        <h1/>
+        <div class="detail">
+          <el-row>
+            <el-col :span="8"/>
+            <el-col :span="3"><span style="font-size: large">要分享的衣物:</span></el-col>
+            <el-col :span="4">
+<!--              <el-input placeholder="请输入衣服的id" v-model="s.scid" clearable class="input_style"></el-input>-->
+              <el-select v-model="s.scid" class="m-2" placeholder="选择衣物" >
+                <el-option
+                    v-for="item in clothe24"
+                    :key="item.cn"
+                    :label="item.cn"
+                    :value="item.cid"
+                />
+              </el-select>
+            </el-col>
+          </el-row>
+          <h1/>
+          <el-row>
+            <el-col :span="8"/>
+            <el-col :span="3"><span style="font-size: larger">标题:</span></el-col>
+            <el-col :span="1">
+              <el-input placeholder="请输入分享的标题" v-model="s.she" clearable class="input_style"></el-input>
+            </el-col>
+          </el-row>
+          <h1/>
+          <el-row>
+            <el-col :span="8"/>
+            <el-col :span="3"><span style="font-size: larger">描述:</span></el-col>
+            <el-col :span="1">
+              <el-input
+                  v-model="s.sde"
+                  :autosize="{ minRows: 2, maxRows: 4 }"
+                  type="textarea"
+                  placeholder="请输入商品的描述"
+                  class="input_style"
+              />
+            </el-col>
+          </el-row>
+        </div>
+        <h1/>
         <div>
-          衣服的id:
-          <el-input placeholder="请输入衣服的id" v-model="s.scid" clearable class="input_style"></el-input>
+          <span>请选择分享类型标签</span>
+          <h1/>
+          <el-checkbox-group v-model="checkTypeList" size="large">
+            <el-checkbox-button v-for="(type,index) in clothe1" :key="index" :label="type.value">
+              {{ type.label }}
+            </el-checkbox-button>
+          </el-checkbox-group>
         </div>
-        <div>
-          分享的标题:
-          <el-input placeholder="请输入分享的标题" v-model="s.she" clearable class="input_style"></el-input>
-        </div>
-        <div>
-          分享的描述:
-          <el-input
-              v-model="s.sde"
-              :autosize="{ minRows: 2, maxRows: 4 }"
-              type="textarea"
-              placeholder="请输入商品的描述"
-              class="input_style"
-          />
-        </div>
-        <el-checkbox-group v-model="checkTypeList" size="large">
-          <el-checkbox-button v-for="(type,index) in clothe1" :key="index" :label="type.value">
-            {{ type.label }}
-          </el-checkbox-button>
-        </el-checkbox-group>
+        <h2/>
 
       </el-card>
       <h6></h6>
@@ -42,11 +82,10 @@
     </el-main>
     <el-footer>
       <div>
-        <el-button @click="upshare">上传分享</el-button>
+        <el-button @click="upshare" type="success">上传分享</el-button>
       </div>
     </el-footer>
   </el-container>
-
 
 </template>
 
@@ -62,6 +101,7 @@ export default {
       s: {sco: "", sid: "", spi: "", sst: 0, scid: "", she: "", sde: "", sti: "", spid: ""},
       dialogOfUpload: false,
       fileList: [],
+      clothe24:[],
       headers: {
         'Content-Type': 'multipart/form-data'
       },
@@ -70,7 +110,53 @@ export default {
       clothe1: store.state.clothTypeList
     }
   },
+  created() {
+    this.query()
+  },
   methods: {
+    query(){
+      this.$axios({
+        method: 'post',
+        url: '/find_all_cloths',
+        data: qs.stringify({
+          cpid:store.state.logInfo.user_id,
+        }),
+        timeout: 1000,
+      })
+          .then(res => {
+            this.clothe24=[]
+            for (let i = 0; i < res.data.result.length ; i++) {
+              let o= {}
+              o.cid = res.data.result[i].cid
+              o.cpid = res.data.result[i].cpid
+              o.cpi = res.data.result[i].cpi
+              o.cpr = res.data.result[i].cpr
+              o.cn = res.data.result[i].cn
+              o.cde = res.data.result[i].cde
+              o.cnum = res.data.result[i].cnum
+              this.clothe24.push(o)
+            }
+          }).catch(err => {
+            console.log(err)
+          }
+      )
+    },
+    upload(file) {
+      const formData = new FormData();
+      formData.append("image", file.file)
+      this.$axios.post('https://api.imgbb.com/1/upload', formData, {
+        params: {
+          key: "b3af80c7860822bee54611e28f1261e2",
+        }
+      }).then((res) => {
+        console.log(res.data.data.url);
+        this.s.spi = res.data.data.url
+        ElMessage.success("图片上传成功！")
+      }).catch(err => {
+        console.log(err)
+        ElMessage.error("图床服务器响应失败！")
+      })
+    },
     upshare: function () {
       //确认上传
       let ccolist = []
